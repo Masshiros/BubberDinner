c0810f22e794c0a615d9d6d4bce76d07085c6c1c
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-03-22 15:23:29 +0700

    FEAT(exception): Handling exception using UseExceptionHandler + ProblemDetails

---
+++ src/BubberDinner.API/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BubberDinner.API.Controllers
+{
+
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [Route("error")]
+        public IActionResult Error()
+        {
+            Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>().Error;
+            return Problem(statusCode:400, title:exception?.Message);
+        }
+    }
+}
---
+++ src/BubberDinner.API/Filter/ErrorHandlingFilterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BubberDinner.API.Filter
+{
+    public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            var exception = context.Exception;
+            var problemDetails = new ProblemDetails
+            {
+                Title = "An error occurred while processing your request",
+                Status = (int)HttpStatusCode.InternalServerError,
+            };
+            context.Result = new ObjectResult(problemDetails)
+            {
+                StatusCode = 500
+            }; 
+            context.ExceptionHandled = true;
+
+        }
+    }
+}
